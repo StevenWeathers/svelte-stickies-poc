1527345ec52b9fc27c6ccefd89e8534220a2cc5f
Steven Weathers <steven@weathers.me>
2019-05-08 00:12:53 -0400

    Prevent column add from shrinking width

--- src/App.svelte
+++ src/App.svelte
@@ -135,7 +135,7 @@
 				{/each}
 			</div>
 		{/each}
-		<div class="m-3 bg-grey-lightest w-24 self-stretch">
+		<div class="m-3 bg-grey-lightest w-24 self-stretch flex-no-shrink">
 			<button on:click={addNoteColumn} class="w-full  h-full font-bold text-xl">
 				<svg class="fill-current text-grey" width="48" height="48" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M845 1395l454-454q19-19 19-45t-19-45l-454-454q-19-19-45-19t-45 19l-102 102q-19 19-19 45t19 45l307 307-307 307q-19 19-19 45t19 45l102 102q19 19 45 19t45-19zm819-499q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z"/></svg>
 			</button>
