7e575498340437eec40dcac3314ff7e1d0ae555a
Steven Weathers <steven@weathers.me>
2019-05-08 21:20:16 -0400

    Add lots of little features, yay

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,10 @@
 <script>
+	import guid from './generateGuid.js'
+
+	import NavMenu from './components/NavMenu.svelte'
+	import TimesIcon from './components/TimesIcon.svelte'
+	import DropperIcon from './components/DropperIcon.svelte'
+
 	const cardColors = [
 		"red",
 		"orange",
@@ -11,41 +17,17 @@
 		"pink",
 	]
 
-	let defaultColor = "blue"
+	const defaultColor = "blue"
 
-	/**
-	* @method: guid
-	* @desc: Generates unique guid
-	**/
-	function guid() {
-		function s4() {
-			return Math.floor((1 + Math.random()) * 0x10000)
-			.toString(16)
-			.substring(1);
-		}
-		return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-			s4() + '-' + s4() + s4() + s4();
-	}
-
+	// All the Stateful things...
 	let notesColumns = [
-		[]
+		{
+			id: guid(),
+			notes: []
+		}
 	]
 
-	function addNote(index) {
-		return () => {
-			const id = guid()
-			notesColumns[index][notesColumns[index].length] = {
-				id,
-				color: defaultColor,
-				title: "Note Title Here",
-				content: `Note Content Here ${id}`,
-			}
-		}
-	}
-
-	function addNoteColumn() {
-		notesColumns[notesColumns.length] = []
-	}
+	let changeColor = ''
 
 	let dragging = {
 		id: '',
@@ -55,7 +37,40 @@
 		target: null,
 	}
 
-	let startDrag = (
+	// event handlers
+	const addNote = (index) => () => {
+		const id = guid()
+		
+		notesColumns[index].notes[notesColumns[index].notes.length] = {
+			id,
+			color: defaultColor,
+			title: "Note Title Here",
+			content: `Note Content Here ${id}`,
+		}
+	}
+
+	const deleteNote = (column, index) => () => {
+		notesColumns[column].notes.splice(notesColumns[column].notes[index], 1)
+		notesColumns = notesColumns
+	}
+
+	const addNoteColumn = () => notesColumns[notesColumns.length] = {
+		id: guid(),
+		notes: [],
+	}
+
+	const showChangeColor = id => () => changeColor = id
+
+	const changeNoteColor = (column, index, color) => () => {
+		notesColumns[column].notes[index].color = color
+		changeColor = ''
+	}
+
+	const noteContentEdit = (column, index, type) => event => {
+		notesColumns[column].notes[index][type] = event.target.value
+	}
+
+	const startDrag = (
 		id,
 		currentCol
 	) => ev =>  {
@@ -70,20 +85,20 @@
 		ev.dataTransfer.effectAllowed = "move"
 	}
 
-	let handleDrop = index => ev => {
+	const handleDrop = index => ev => {
 		ev.preventDefault()
 
-		const finalIndex = dragging.order || notesColumns[index].length
+		const finalIndex = dragging.order || notesColumns[index].notes.length
 		
-		const note = notesColumns[dragging.currentCol].find((n) => n.id === dragging.id)
+		const note = notesColumns[dragging.currentCol].notes.find((n) => n.id === dragging.id)
 
-		notesColumns[dragging.currentCol].splice(notesColumns[dragging.currentCol].indexOf(note), 1)
-		notesColumns[index].splice(finalIndex, 0, note)
+		notesColumns[dragging.currentCol].notes.splice(notesColumns[dragging.currentCol].notes.indexOf(note), 1)
+		notesColumns[index].notes.splice(finalIndex, 0, note)
 
 		notesColumns = notesColumns
 	}
 
-	let handleDragOver = ev => {
+	const handleDragOver = ev => {
 		if (ev.preventDefault) {
 			ev.preventDefault(); // Necessary. Allows us to drop.
 		}
@@ -103,41 +118,48 @@
 </style>
 
 <div class="h-screen">
-	<nav class="flex items-center justify-between bg-teal p-6">
-		<div class="flex items-center flex-no-shrink text-white mr-6">
-			<span class="font-semibold text-xl tracking-tight">Sticky Note POC</span>
-		</div>
-		<div class="block lg:hidden">
-			<button class="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white">
-			<svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-			</button>
-		</div>
-	</nav>
-
+	<NavMenu />
 
 	<section class="flex items-stretch min-h-full" style="overflow-x: scroll;">
 		{#each notesColumns as noteColumn, index}
 			<div class="flex-no-shrink m-3" style="width: 320px;" on:dragover={handleDragOver} on:drop={handleDrop(index)} data-droppable="true">
-				<button on:click={addNote(index)} class="w-full font-bold text-xl text-grey">
+				<button on:click={addNote(index)} class="w-full font-bold text-3xl text-grey bg-grey-lightest p-1">
 					+
 				</button>
-				{#each noteColumn as note, i (note.id)}
-					<div class="max-w-xs rounded shadow bg-{note.color}-lightest border border-{note.color}-lighter mt-5" data-index="{i}" id="{note.id}" on:dragover={handleDragOver} draggable on:dragstart={startDrag(note.id, index)}>
-						<div class="px-6 py-4">
-							<div class="font-bold text-xl mb-2">
-								{note.title}
+				{#each noteColumn.notes as note, i (note.id)}
+					<div class="max-w-xs shadow bg-{note.color}-lightest border border-{note.color}-lighter mt-5" data-index="{i}" id="{note.id}" on:dragover={handleDragOver} draggable on:dragstart={startDrag(note.id, index)}>
+						<div class="p-3">
+							<div class="mb-2 relative flex mb-4">
+								<div class="w-1/4">
+									<button class="float-left" on:click={deleteNote(index, i)}>
+										<TimesIcon color={note.color} />
+									</button>
+								</div>
+								<div class="w-2/4">
+									<input type="text" value="{note.title}" on:change={noteContentEdit(index, i, "title")} class="inline font-bold text-xl bg-transparent" />
+								</div>
+								<div class="w-1/4 text-right">
+									<button on:click={showChangeColor(note.id)}>
+										<DropperIcon color={note.color} />
+									</button>
+									{#if changeColor === note.id}
+										<div class="shadow border bg-white absolute pin-r">
+											{#each cardColors as color}
+												<button on:click={changeNoteColor(index, i, color)} class="p-3 hover:bg-{color}-lighter bg-{color}-lightest">&nbsp;</button>
+											{/each}
+										</div>
+									{/if}
+								</div>
 							</div>
-							<p class="text-grey-darker text-base">
-								{note.content}
-							</p>
+							<textarea class="w-full h-full bg-transparent" rows="5" on:change={noteContentEdit(index, i, "content")}>{note.content}</textarea>
 						</div>
 					</div>
 				{/each}
 			</div>
 		{/each}
-		<div class="m-3 bg-grey-lightest w-24 self-stretch flex-no-shrink">
-			<button on:click={addNoteColumn} class="w-full  h-full font-bold text-xl">
-				<svg class="fill-current text-grey" width="48" height="48" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M845 1395l454-454q19-19 19-45t-19-45l-454-454q-19-19-45-19t-45 19l-102 102q-19 19-19 45t19 45l307 307-307 307q-19 19-19 45t19 45l102 102q19 19 45 19t45-19zm819-499q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z"/></svg>
+		<div class="m-3 bg-grey-lightest w-16 self-stretch flex-no-shrink">
+			<button on:click={addNoteColumn} class="w-full h-full font-bold text-5xl text-grey">
+				+
 			</button>
 		</div>
 	</section>
---
+++ src/components/NavMenu.svelte
@@ -0,0 +1,5 @@
+<nav class="flex items-center justify-between bg-teal p-6">
+    <div class="flex items-center flex-no-shrink text-white mr-6">
+        <span class="font-semibold text-xl tracking-tight">Sticky Note POC</span>
+    </div>
+</nav>
