81774a96e00ba5a32f4f230dfc5f2d514a40533b
Steven Weathers <steven@weathers.me>
2019-06-06 22:21:09 -0400

    Replace HTML5 drag/drop with Dragula
    Because so much smoother...

--- src/App.svelte
+++ src/App.svelte
@@ -1,195 +1,229 @@
 <script>
-	import guid from './generateGuid.js'
+  import dragula from "dragula";
 
-	import NavMenu from './components/NavMenu.svelte'
-	import TimesIcon from './components/TimesIcon.svelte'
-	import DropperIcon from './components/DropperIcon.svelte'
+  import guid from "./generateGuid.js";
 
-	const cardColors = [
-		"red",
-		"orange",
-		"yellow",
-		"green",
-		"teal",
-		"blue",
-		"indigo",
-		"purple",
-		"pink",
-	]
+  import NavMenu from "./components/NavMenu.svelte";
+  import TimesIcon from "./components/TimesIcon.svelte";
+  import DropperIcon from "./components/DropperIcon.svelte";
 
-	const defaultColor = "blue"
+  const cardColors = [
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "teal",
+    "blue",
+    "indigo",
+    "purple",
+    "pink"
+  ];
 
-	// All the Stateful things...
-	let notesColumns = [
-		{
-			id: guid(),
-			notes: []
-		}
-	]
+  const defaultColor = "blue";
 
-	let changeColor = ''
+  // instantiate dragula, utilizing drop-column as class for the containers
+  const drake = dragula({
+    isContainer: function(el) {
+      return el.classList.contains("drop-column");
+    }
+  });
 
-	let dragging = {
-		id: '',
-		status: false,
-		currentCol: 0,
-		order: null,
-		target: null,
-	}
+  // All the Stateful things...
+  let notesColumns = [
+    {
+      id: guid(),
+      notes: []
+    }
+  ];
 
-	// event handlers
-	const makeEditable = ev => {
-		ev.target.removeAttribute('readonly')
-	}
-	const addNote = (index) => () => {
-		const id = guid()
+  let changeColor = "";
 
-		notesColumns[index].notes[notesColumns[index].notes.length] = {
-			id,
-			color: defaultColor,
-			title: "Note Title Here",
-			content: `Note Content Here ${id}`,
-			contentHeight: null,
-		}
-	}
+  // event handlers
+  const addNote = index => () => {
+    const id = guid();
 
-	const deleteNote = (column, index) => () => {
-		notesColumns[column].notes.splice(notesColumns[column].notes[index], 1)
-		notesColumns = notesColumns
-	}
+    notesColumns[index].notes[notesColumns[index].notes.length] = {
+      id,
+      color: defaultColor,
+      title: "Note Title Here",
+      content: `Note Content Here ${id}`,
+      contentHeight: null
+    };
+  };
 
-	const addNoteColumn = () => notesColumns[notesColumns.length] = {
-		id: guid(),
-		notes: [],
-	}
+  const deleteNote = (column, index) => () => {
+    notesColumns[column].notes.splice(notesColumns[column].notes[index], 1);
+    notesColumns = notesColumns;
+  };
 
-	const showChangeColor = id => () => changeColor = id
+  const addNoteColumn = () => {
+    notesColumns[notesColumns.length] = {
+      id: guid(),
+      notes: []
+    };
+  };
 
-	const changeNoteColor = (column, index, color) => () => {
-		notesColumns[column].notes[index].color = color
-		changeColor = ''
-	}
+  const showChangeColor = id => () => (changeColor = id);
 
-	const noteContentEdit = (column, index, type) => event => {
-		notesColumns[column].notes[index][type] = event.target.value
-		event.target.setAttribute('readonly', true)
-	}
+  const changeNoteColor = (column, index, color) => () => {
+    notesColumns[column].notes[index].color = color;
+    changeColor = "";
+  };
 
-	const startDrag = (
-		id,
-		currentCol
-	) => ev =>  {
-		dragging = {
-			id,
-			currentCol,
-			status: true,
-			target: ev.target,
-			order: ev.target.dataset.index
-		}
+  const noteContentEdit = (column, index, type) => event => {
+    notesColumns[column].notes[index][type] = event.target.value;
+  };
 
-		ev.dataTransfer.effectAllowed = "move"
-	}
+  drake.on("drop", function(el, target, source, sibling) {
+    const noteId = el.dataset.noteid;
+    const targetColIndex = target.dataset.columnindex;
+    const sourceColIndex = source.dataset.columnindex;
 
-	const handleDrop = index => ev => {
-		ev.preventDefault()
+    // determine what order to place note in column
+    const finalIndex = sibling
+      ? sibling.dataset.index
+      : notesColumns[targetColIndex].notes.length;
 
-		const finalIndex = dragging.order || notesColumns[index].notes.length
-		
-		const note = notesColumns[dragging.currentCol].notes.find((n) => n.id === dragging.id)
+    const note = notesColumns[sourceColIndex].notes.find(n => n.id === noteId);
 
-		notesColumns[dragging.currentCol].notes.splice(notesColumns[dragging.currentCol].notes.indexOf(note), 1)
-		notesColumns[index].notes.splice(finalIndex, 0, note)
+    // remote note from source column
+    notesColumns[sourceColIndex].notes.splice(
+      notesColumns[sourceColIndex].notes.indexOf(note),
+      1
+    );
 
-		notesColumns = notesColumns
-	}
+    // add note to target column
+    notesColumns[targetColIndex].notes.splice(finalIndex, 0, note);
 
-	const handleDragOver = ev => {
-		if (ev.preventDefault) {
-			ev.preventDefault() // Necessary. Allows us to drop.
-		}
+    // finally update state
+    notesColumns = notesColumns;
+  });
 
-		if (ev.target.dataset.droppable != "true")
-			ev.dataTransfer.dropEffect = "none" // dropping is not allowed
-		else
-			ev.dataTransfer.dropEffect = "move" // drop it like it's hot
+  // catch and update textarea adjusted size in data
+  const detectElementMouseEnlargement = (column, index) => ev => {
+    const element = ev.target;
+    const size = { height: element.clientHeight };
+    let styleHeight = parseFloat(
+      getComputedStyle(element)["height"].replace("px", "")
+    );
 
-		if (ev.target.id) {
-			dragging.order = ev.target.dataset.index
-		}
-	}
+    const mouseMoveListener = event => {
+      if (element.clientHeight != size.height) {
+        let style = getComputedStyle(element);
+        styleHeight = parseFloat(style["height"].replace("px", ""));
 
-	const detectElementMouseEnlargement = (column, index) => ev => {
-		const element = ev.target
-		const size = { height: element.clientHeight }
-		let styleHeight = parseFloat(getComputedStyle(element)['height'].replace('px', ''))
-		
-		const mouseMoveListener = event => {
-			if (element.clientHeight != size.height) {
-				let style = getComputedStyle(element)
-				styleHeight = parseFloat(style['height'].replace('px', ''))
-				
-				size.height = element.clientHeight
-			}
-		}
-		
-		const mouseUpListener = event => {
-			window.removeEventListener("mousemove", mouseMoveListener)
-			window.removeEventListener("mouseup", mouseUpListener)
-			notesColumns[column].notes[index].contentHeight = styleHeight
-		}
-		
-		window.addEventListener("mousemove", mouseMoveListener)
-		window.addEventListener("mouseup", mouseUpListener)
-	}
+        size.height = element.clientHeight;
+      }
+    };
+
+    const mouseUpListener = event => {
+      window.removeEventListener("mousemove", mouseMoveListener);
+      window.removeEventListener("mouseup", mouseUpListener);
+      notesColumns[column].notes[index].contentHeight = styleHeight;
+    };
+
+    window.addEventListener("mousemove", mouseMoveListener);
+    window.addEventListener("mouseup", mouseUpListener);
+  };
 </script>
 
 <style>
+  /** Manually including Dragula styles, should automate this later */
+  :global(.gu-mirror) {
+    position: fixed !important;
+    margin: 0 !important;
+    z-index: 9999 !important;
+    opacity: 0.8;
+    -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=80)";
+    filter: alpha(opacity=80);
+  }
+
+  :global(.gu-hide) {
+    display: none !important;
+  }
+
+  :global(.gu-unselectable) {
+    -webkit-user-select: none !important;
+    -moz-user-select: none !important;
+    -ms-user-select: none !important;
+    user-select: none !important;
+  }
+
+  :global(.gu-transit) {
+    opacity: 0.2;
+    -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=20)";
+    filter: alpha(opacity=20);
+  }
 </style>
 
 <div class="h-screen">
-	<NavMenu />
+  <NavMenu />
 
-	<section class="flex items-stretch min-h-full" style="overflow-x: scroll">
-		{#each notesColumns as noteColumn, index}
-			<div class="flex-no-shrink m-3" style="width: 320px" on:dragover={handleDragOver} on:drop={handleDrop(index)} data-droppable="true">
-				<button on:click={addNote(index)} class="w-full font-bold text-3xl text-grey bg-grey-lightest p-1">
-					+
-				</button>
-				{#each noteColumn.notes as note, i (note.id)}
-					<div class="max-w-xs shadow bg-{note.color}-lightest border border-{note.color}-lighter mt-5" data-index="{i}" id="{note.id}" on:dragover={handleDragOver} draggable on:dragstart={startDrag(note.id, index)}>
-						<div class="p-3">
-							<div class="mb-2 relative flex mb-4">
-								<div class="w-1/4">
-									<button class="float-left" on:click={deleteNote(index, i)}>
-										<TimesIcon color={note.color} />
-									</button>
-								</div>
-								<div class="w-2/4">
-									<input readonly="readonly" type="text" value="{note.title}" on:dblclick={makeEditable} on:change={noteContentEdit(index, i, "title")} class="inline font-bold text-xl bg-transparent" />
-								</div>
-								<div class="w-1/4 text-right">
-									<button on:click={showChangeColor(note.id)}>
-										<DropperIcon color={note.color} />
-									</button>
-									{#if changeColor === note.id}
-										<div class="shadow border bg-white absolute pin-r">
-											{#each cardColors as color}
-												<button on:click={changeNoteColor(index, i, color)} class="p-3 hover:bg-{color}-lighter bg-{color}-lightest">&nbsp;</button>
-											{/each}
-										</div>
-									{/if}
-								</div>
-							</div>
-							<textarea style="height: {note.contentHeight ? `${note.contentHeight}px` : 'auto'}" on:mousedown={detectElementMouseEnlargement(index, i)} class="w-full h-full bg-transparent" rows="5" on:change={noteContentEdit(index, i, "content")} readonly="readonly" on:dblclick={makeEditable}>{note.content}</textarea>
-						</div>
-					</div>
-				{/each}
-			</div>
-		{/each}
-		<div class="m-3 bg-grey-lightest w-16 self-stretch flex-no-shrink">
-			<button on:click={addNoteColumn} class="w-full h-full font-bold text-5xl text-grey">
-				+
-			</button>
-		</div>
-	</section>
+  <section class="flex items-stretch min-h-full" style="overflow-x: scroll">
+    {#each notesColumns as noteColumn, index}
+      <div class="flex-no-shrink m-3" style="width: 320px">
+        <button
+          on:click={addNote(index)}
+          class="w-full font-bold text-3xl text-grey bg-grey-lightest p-1">
+          +
+        </button>
+        <ul
+          class="drop-column list-reset w-full min-h-full"
+          data-columnid={noteColumn.id}
+          data-columnindex={index}>
+          {#each noteColumn.notes as note, i (note.id)}
+            <li
+              class="max-w-xs shadow bg-{note.color}-lightest border border-{note.color}-lighter
+              mt-5 list-reset"
+              data-index={i}
+              data-noteid={note.id}>
+              <div class="p-3">
+                <div class="mb-2 relative flex mb-4">
+                  <div class="w-1/4">
+                    <button class="float-left" on:click={deleteNote(index, i)}>
+                      <TimesIcon color={note.color} />
+                    </button>
+                  </div>
+                  <div class="w-2/4">
+                    <input
+                      type="text"
+                      value={note.title}
+                      on:change={noteContentEdit(index, i, 'title')}
+                      class="inline font-bold text-xl bg-transparent" />
+                  </div>
+                  <div class="w-1/4 text-right">
+                    <button on:click={showChangeColor(note.id)}>
+                      <DropperIcon color={note.color} />
+                    </button>
+                    {#if changeColor === note.id}
+                      <div class="shadow border bg-white absolute pin-r">
+                        {#each cardColors as color}
+                          <button
+                            on:click={changeNoteColor(index, i, color)}
+                            class="p-3 hover:bg-{color}-lighter bg-{color}-lightest" />
+                        {/each}
+                      </div>
+                    {/if}
+                  </div>
+                </div>
+                <textarea
+                  style="height: {note.contentHeight ? `${note.contentHeight}px` : 'auto'}"
+                  on:mousedown={detectElementMouseEnlargement(index, i)}
+                  class="w-full h-full bg-transparent"
+                  rows="5"
+                  on:change={noteContentEdit(index, i, 'content')}>{note.content}</textarea>
+              </div>
+            </li>
+          {/each}
+        </ul>
+      </div>
+    {/each}
+    <div class="m-3 bg-grey-lightest w-16 self-stretch flex-no-shrink">
+      <button
+        on:click={addNoteColumn}
+        class="w-full h-full font-bold text-5xl text-grey">
+        +
+      </button>
+    </div>
+  </section>
 </div>
